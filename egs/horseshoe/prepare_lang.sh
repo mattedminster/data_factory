273302f9ae1d19e57e7093ec9b54e8d5674221c7
matt <matt@rengegadelabs.io>
2020-06-20 21:35:33 -0400

    #2 here we go

--- egs/horseshoe/prepare_lang.sh
+++ egs/horseshoe/prepare_lang.sh
@@ -1,5 +1,3 @@
 #! /bin/bash
-utils/prepare_lang.sh data/local/lang 'OOV' data/local/ data/lang
+utils/prepare_lang.sh data/local/lang '<oov>' data/local/ data/lang
                     
-# where the underlying argument structure is:
-utils/prepare_lang.sh <dict-src-dir> <oov-dict-entry> <tmp-dir> <lang-dir>

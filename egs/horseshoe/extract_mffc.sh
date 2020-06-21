bb6dbffd86f0066c4cab44f491d0d5bcddbbb18b
matt <matt@rengegadelabs.io>
2020-06-20 22:14:39 -0400

    #2 here we go

--- egs/horseshoe/extract_mffc.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-mfccdir=mfcc  
-x=data/train  
-steps/make_mfcc.sh --cmd "run.pl" --nj ${params.num_of_jobs} $x exp/make_mfcc/$x $mfccdir  
-steps/compute_cmvn_stats.sh $x exp/make_mfcc/$x $mfccdir
--- egs/horseshoe/monophone_training.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-utils/subset_data_dir.sh --first data/train ${params.monophone_max} data/train_10k
